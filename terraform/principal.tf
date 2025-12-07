9a7216dc6ba4b47f27665a1a0b9b002e94a1550d
Jey <jeymb5737@gmail.com>
2025-12-07 23:40:21 +0100

    update principal.tf

--- terraform/principal.tf
+++ terraform/principal.tf
@@ -9,13 +9,17 @@ terraform {
 
 provider "docker" {}
 
+# Télécharge l’image depuis DockerHub
 resource "docker_image" "demo" {
-  name = "jejey333/demo-devops:latest"
+  name         = "jejey333/demo-devops:latest"
+  keep_locally = false
 }
 
+# Lance un conteneur local basé sur l’image
 resource "docker_container" "demo_container" {
   name  = "demo-devops-container"
-  image = docker_image.demo.name
+  image = docker_image.demo.image_id
+
   ports {
     internal = 80
     external = 8080
